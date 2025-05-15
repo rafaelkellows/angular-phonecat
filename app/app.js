6eb0d9967f95d17ce9f2f8e6046c22b038a39109
unknown <rafaelkellows@hotmail.com>
2025-05-15 17:57:29 -0300

    Estrutura

---
+++ app/app.js
@@ -0,0 +1,22 @@
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+    $scope.name = 'Rafael Kellows';
+    $scope.phones = [
+        {
+        name: 'Nexus S',
+        snippet: 'Fast just got faster with Nexus S.'
+        }, {
+        name: 'Motorola XOOM™ with Wi-Fi',
+        snippet: 'The Next, Next Generation tablet.'
+        }, {
+        name: 'MOTOROLA XOOM™',
+        snippet: 'Next Generation tablet.'
+        }, {
+        name: 'iPhone Ultra',
+        snippet: 'Next iPhone Mobile.'
+        }
+    ];
+});
